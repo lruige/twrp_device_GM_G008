8c134153697b15651c5a4255865ec70176d7c040
Efe Timur TURAN <xpazr@yaani.com>
2024-10-24 23:47:26 +0300

    Delete omni_GM_G008.mk

--- omni_GM_G008.mk
+++
@@ -1,9 +0,0 @@
-# Inherit device configuration
-$(call inherit-product, device/GM/GM_G008/GM_G008.mk)
-
-# Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := GM_G008
-PRODUCT_NAME := omni_GM_G008
-PRODUCT_BRAND := GM
-PRODUCT_MODEL := General Mobile Various Android GO Device
-PRODUCT_MANUFACTURER := GM
